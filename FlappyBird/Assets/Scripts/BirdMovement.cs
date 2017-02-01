fc04daf51cc1e733576a3758338f5b53fd35e088
Duong Tony <tonyduong@live.fr>
2017-02-01 15:31:19 -0500

    First version

---
+++ FlappyBird/Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+
+    Vector3 velocity = Vector3.zero;
+    public Vector3 gravity;
+    public Vector3 flapVelocity;
+    public float maxSpeed = 5f;
+    public float forwardSpeed = 1f;
+    bool didFlap = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Do graphic & input updates here
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            didFlap = true;
+        }
+    }
+	
+	// Do physics engine update here
+	void FixedUpdate () {
+        velocity.x = forwardSpeed;
+        velocity += gravity * Time.deltaTime;
+        if (didFlap)
+        {
+            didFlap = false;
+            if (velocity.y < 0)
+            {
+                velocity.y = 0;
+            }
+            velocity += flapVelocity;
+        }
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        transform.position += velocity * Time.deltaTime;
+
+        float angle = 0f;
+        if (velocity.y < 0)
+        {
+            angle = Mathf.Lerp(0, -90, -velocity.y / maxSpeed);
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
---
+++ FlappyBird/Assets/Scripts/Tracking.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tracking : MonoBehaviour {
+
+    Transform target;
+    float offsetX;
+	// Use this for initialization
+	void Start () {
+        GameObject target_t = GameObject.FindGameObjectWithTag("Player");
+        if (target_t == null)
+        {
+            Debug.LogError("Couldn't find an object with tag 'Player'!");
+            return;
+        }
+        target = target_t.transform;
+        offsetX = transform.position.x - target.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (target != null)
+        {
+            Vector3 t = gameObject.transform.position;
+            t.x = target.position.x + offsetX;
+            gameObject.transform.position = t;
+        }
+        
+	}
+}
