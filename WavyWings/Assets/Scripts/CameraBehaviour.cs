939e75b83b51ffa90906aa46aba4baee1883ec85
Duong Tony <tonyduong@live.fr>
2017-02-08 13:54:32 -0500

    WavyWings - TinyWings Clone
    - Hills generation
    - Force added to the character
    - Camera and background following the player

---
+++ WavyWings/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject background;
+    public float x_offset;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 leftCamera = new Vector3(0,0,0);
+        x_offset = transform.position.x - player.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newCameraCoordinates = new Vector3(player.transform.position.x + x_offset, 
+            transform.position.y, 
+            transform.position.z);
+
+        Vector3 cameraMovement = newCameraCoordinates - transform.position;
+        
+        // Change the position of the cameras
+        transform.position = newCameraCoordinates;
+
+        // Change the position of the background
+        background.transform.position = background.transform.position + cameraMovement;
+
+
+	}
+}
---
+++ WavyWings/Assets/Scripts/FishBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishBehaviour : MonoBehaviour {
+
+    private Vector2 velocity;
+    private bool isDiving;
+    private Rigidbody2D rb;
+    public float divingForce;
+    public float forwardForce;
+    public float highestSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("down"))
+        {
+            isDiving = true;
+        } else
+        {
+            isDiving = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(1,0) * forwardForce);
+        if (rb.velocity.x > highestSpeed) {
+
+            rb.velocity = new Vector2(highestSpeed, rb.velocity.y);
+        }
+        if (isDiving)
+        {
+            rb.AddForce(transform.up * -1 * divingForce);
+        }
+    }
+}
